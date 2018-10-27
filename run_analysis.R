27f89dcf68f4fe032ae28e93faac1cc297549cd8
malexcaballero <malexcaballero@gmail.com>
2018-10-26 23:42:47 -0700

    last

--- run_analysis.R
+++ run_analysis.R
@@ -39,7 +39,39 @@ finalData <- rbind(trainData,testData)
 colNames <- colnames(finalData)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement
-
-
 data_mean_std <-finalData[,grepl("mean|std|subject|activityId",colnames(finalData))]
+
+#3. #Uses descriptive activity names to name the activities in the data set
+library(plyr)
+data_mean_std <- join(data_mean_std, activityLabel, by = "activityId", match = "first")
+data_mean_std <-data_mean_std[,-1]
+
+#4. Appropriately labels the data set with descriptive variable names.
+
+#Remove parentheses
+
+names(data_mean_std) <- gsub("\\(|\\)", "", names(data_mean_std), perl  = TRUE)
+
+#correct syntax in names
+
+names(data_mean_std) <- make.names(names(data_mean_std))
+
+#add descriptive names
+
+names(data_mean_std) <- gsub("Acc", "acceleration", names(data_mean_std))
+names(data_mean_std) <- gsub("^t", "time", names(data_mean_std))
+names(data_mean_std) <- gsub("^f", "frequency", names(data_mean_std))
+names(data_mean_std) <- gsub("BodyBody", "body", names(data_mean_std))
+names(data_mean_std) <- gsub("mean", "mean", names(data_mean_std))
+names(data_mean_std) <- gsub("std", "std", names(data_mean_std))
+names(data_mean_std) <- gsub("Freq", "frequency", names(data_mean_std))
+names(data_mean_std) <- gsub("Mag", "magnitude", names(data_mean_std))
+
+#creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+tidydata_average_sub<- ddply(data_mean_std, c("subject","activity"), numcolwise(mean))
+
+
+write.table(tidydata_average_sub,file="tidydata.txt",row.names = FALSE)
 
