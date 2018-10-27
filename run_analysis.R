a3c30e0ca30e154b089fb63538b63d41830d9bf5
malexcaballero <malexcaballero@gmail.com>
2018-10-26 23:21:24 -0700

    segunda parte

--- run_analysis.R
+++ run_analysis.R
@@ -15,7 +15,9 @@ activityLabel   <- read.table("./activity_labels.txt",header=FALSE)
 subjectTrain    <-read.table("./train/subject_train.txt", header=FALSE)
 xTrain          <- read.table("./train/X_train.txt", header=FALSE)
 yTrain          <- read.table("./train/y_train.txt", header=FALSE)
-
+subjectTest    <-read.table("./test/subject_test.txt", header=FALSE)
+xTest         <- read.table("./test/X_test.txt", header=FALSE)
+yTest         <- read.table("./test/y_test.txt", header=FALSE)
 
 #Assign column names 
 
@@ -23,34 +25,17 @@ colnames(activityLabel)<-c("activityId","activityType")
 colnames(subjectTrain) <- "subId"
 colnames(xTrain) <- features[,2]
 colnames(yTrain) <- "activityId"
-
-
-#Merging training Data.
-
-trainData <- cbind(yTrain,subjectTrain,xTrain)
-
-#Reading Data
-
-subjectTest    <-read.table("./test/subject_test.txt", header=FALSE)
-xTest         <- read.table("./test/X_test.txt", header=FALSE)
-yTest         <- read.table("./test/y_test.txt", header=FALSE)
-
-# Assign column names
-
 colnames(subjectTest) <- "subId"
 colnames(xTest) <- features[,2]
 colnames(yTest) <- "activityId"
 
-# merging test Data
+#Merging training Data.
+
+trainData <- cbind(yTrain,subjectTrain,xTrain)
 testData <- cbind(yTest,subjectTest,xTest)
-
-
-#final merged data
-
 finalData <- rbind(trainData,testData)
 
-# creating a vector for column names to be used further
-
+#vector for column
 colNames <- colnames(finalData)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement
