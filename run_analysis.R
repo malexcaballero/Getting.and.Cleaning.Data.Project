0ad13af89b3384d4d34fecafb6eb604eb15b17f4
malexcaballero <malexcaballero@gmail.com>
2018-10-26 22:51:54 -0700

    primera parte

---
+++ run_analysis.R
@@ -0,0 +1,55 @@
+#######################################################################
+##
+##Data
+
+#https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+#Set Working Directory
+
+#1. Merge the training and the test data.
+
+#reading the data 
+
+features        <- read.table("./features.txt",header=FALSE)
+activityLabel   <- read.table("./activity_labels.txt",header=FALSE)
+subjectTrain    <-read.table("./train/subject_train.txt", header=FALSE)
+xTrain          <- read.table("./train/X_train.txt", header=FALSE)
+yTrain          <- read.table("./train/y_train.txt", header=FALSE)
+
+
+#Assign column names 
+
+colnames(activityLabel)<-c("activityId","activityType")
+colnames(subjectTrain) <- "subId"
+colnames(xTrain) <- features[,2]
+colnames(yTrain) <- "activityId"
+
+
+#Merging training Data.
+
+trainData <- cbind(yTrain,subjectTrain,xTrain)
+
+#Reading Data
+
+subjectTest    <-read.table("./test/subject_test.txt", header=FALSE)
+xTest         <- read.table("./test/X_test.txt", header=FALSE)
+yTest         <- read.table("./test/y_test.txt", header=FALSE)
+
+# Assign column names
+
+colnames(subjectTest) <- "subId"
+colnames(xTest) <- features[,2]
+colnames(yTest) <- "activityId"
+
+# merging test Data
+testData <- cbind(yTest,subjectTest,xTest)
+
+
+#final merged data
+
+finalData <- rbind(trainData,testData)
+
+# creating a vector for column names to be used further
+
+colNames <- colnames(finalData)
+
